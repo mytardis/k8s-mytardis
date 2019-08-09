81a6875c2b8636f21a329b084a5e3ebe86a1329e
James Wettenhall <james.wettenhall@gmail.com>
2019-08-09 12:24:31 +1000

    Assert that settings.yaml exists, instead of using an if block, because as we add more settings to settings.yaml which need to be loaded into settings.py, it becomes more difficult to keep track of what should be indented in the if block in settings.py

--- settings.py
+++ settings.py
@@ -10,45 +10,45 @@ from .default_settings import *
 
 settings_filename = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                  'settings.yaml')
-if os.path.isfile(settings_filename):
-    with open(settings_filename) as settings_file:
-        data = yaml.load(settings_file, Loader=yaml.FullLoader)
+assert os.path.isfile(settings_filename)
+with open(settings_filename) as settings_file:
+    data = yaml.load(settings_file, Loader=yaml.FullLoader)
 
-    DEBUG = data['debug']
-    SECRET_KEY = data['secret_key']
+DEBUG = data['debug']
+SECRET_KEY = data['secret_key']
 
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': data['postgres']['host'],
-        'PORT': data['postgres']['port'],
-        'USER': data['postgres']['user'],
-        'PASSWORD': data['postgres']['password'],
-        'NAME': data['postgres']['name']
-    }
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'HOST': data['postgres']['host'],
+    'PORT': data['postgres']['port'],
+    'USER': data['postgres']['user'],
+    'PASSWORD': data['postgres']['password'],
+    'NAME': data['postgres']['name']
+}
 
-    CELERY_RESULT_BACKEND = 'amqp'
-    BROKER_URL = 'amqp://%(user)s:%(password)s@%(host)s:%(port)s/%(vhost)s' % {
-        'host': data['rabbitmq']['host'],
-        'port': data['rabbitmq']['port'],
-        'user': data['rabbitmq']['user'],
-        'password': urllib.quote_plus(data['rabbitmq']['password'],),
-        'vhost': data['rabbitmq']['vhost']
-    }
+CELERY_RESULT_BACKEND = 'amqp'
+BROKER_URL = 'amqp://%(user)s:%(password)s@%(host)s:%(port)s/%(vhost)s' % {
+    'host': data['rabbitmq']['host'],
+    'port': data['rabbitmq']['port'],
+    'user': data['rabbitmq']['user'],
+    'password': urllib.quote_plus(data['rabbitmq']['password'],),
+    'vhost': data['rabbitmq']['vhost']
+}
 
-    DEFAULT_STORAGE_BASE_DIR = data['default_store_path']
-    METADATA_STORE_PATH = data['metadata_store_path']
+DEFAULT_STORAGE_BASE_DIR = data['default_store_path']
+METADATA_STORE_PATH = data['metadata_store_path']
 
-    EMAIL_HOST = data['email']['host']
-    EMAIL_PORT = data['email']['port']
-    DEFAULT_FROM_EMAIL = data['email']['from']
-    SERVER_EMAIL = data['email']['server']
+EMAIL_HOST = data['email']['host']
+EMAIL_PORT = data['email']['port']
+DEFAULT_FROM_EMAIL = data['email']['from']
+SERVER_EMAIL = data['email']['server']
 
-    ADMINS = []
-    for user in data['admins']:
-        ADMINS.append((user['name'], user['email']))
-    MANAGERS = ADMINS
+ADMINS = []
+for user in data['admins']:
+    ADMINS.append((user['name'], user['email']))
+MANAGERS = ADMINS
 
-    INSTALLED_APPS += tuple(data['installed_apps'])
+INSTALLED_APPS += tuple(data['installed_apps'])
 
 
 CELERY_QUEUES += (
